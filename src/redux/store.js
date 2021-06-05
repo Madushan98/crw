3d072e41bf6177c50c25e0ee35587a72cdac8435
Madushan <ranasinhemadushan@gmail.com>
2021-06-05 14:37:35 +0530

    heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,8 +5,12 @@ import { persistStore } from 'redux-persist';
 
 
 
-const middleWares = [logger];
+const middleWares = [];
 
+
+if (process.env.NODE_ENV === 'development') {
+    middleWares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
@@ -14,8 +18,6 @@ export const persistor = persistStore(store);
 
 
 export default { store, persistor };
-
-
 
 
 
